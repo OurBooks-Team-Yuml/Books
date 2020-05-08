d338c81714e9c5bbeceb03cafcc49a811ccc4481
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-05-08 14:38:29 +0200

    Add base repositories

---
+++ books/repositories/books.py
@@ -0,0 +1,7 @@
+from books.database import Book as BookDB
+from books.entities import Book
+from books.use_cases.repositories import BaseBookRepository
+
+
+class BookRepository(BaseBookRepository):
+    pass
--- books/repository.py
+++
@@ -1,9 +0,0 @@
-from .database import books_coll
-from .entities import Book
-
-class BaseBookRepository(object):
-    pass
-
-
-class BookRepository(BaseBookRepository):
-    pass
