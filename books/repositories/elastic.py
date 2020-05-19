ae6e2c762848fcb76feb520576b0463d196f2baf
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-05-19 08:36:27 +0200

    Add author's full name to elastic document

--- books/repositories/elastic.py
+++ books/repositories/elastic.py
@@ -13,8 +13,11 @@ class ElasticRepository(BaseElasticRepository):
         es.index(index=os.environ['ES_BOOKS_INDEX'], id=book.id, body=asdict(book))
 
     def add_author(self, author: Author) -> None:
+        full_name = f"{author.first_name} {author.last_name}"
+        body = {**asdict(author), "full_name": full_name}
+
         es = self._get_elastic()
-        es.index(index=os.environ['ES_AUTHORS_INDEX'], id=author.id, body=asdict(author))
+        es.index(index=os.environ['ES_AUTHORS_INDEX'], id=author.id, body=body)
 
     def _get_elastic(self) -> Elasticsearch:
         return Elasticsearch(os.environ['ES_URL'])
